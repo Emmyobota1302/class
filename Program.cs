3bdf8a7f87ded22817d4fe0bfd55cbbab2f64de1
emmyobota1302 <obotaemelda@gmail.com>
2022-06-30 18:44:18 +0300

    Class

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Defining{
+    class Program{
+        static void Main(string[] args){
+            Book b1=new Book("war and peace","leo",825);
+            Book b2=new Book("desire","emmy",435);
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
+            
+        }
+    }
+}
